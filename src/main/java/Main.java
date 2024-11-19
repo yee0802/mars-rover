a35a7b1d6f2c58dfe9541c5589e58418060b8a53
yee0802 <NovusAa12@proton.me>
2024-11-19 12:20:32 +0000

    feat: integrate MissionControl into main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import ui.MissionControl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MissionControl missionControl = new MissionControl();
+
+        missionControl.launch();
     }
 }
